6b5795a49d34a8d00d16d5009d417210492202ce
DjZengo101 <utkarshsharma2208@gmail.com>
2023-07-11 15:54:25 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const collegeData = require("./modules/collegeData");
+
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+// Middleware to parse JSON data
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route to get all students or students by course
+app.get("/students", (req, res) => {
+  const course = req.query.course;
+  console.log(course);
+  if (course) {
+    collegeData.getStudentsByCourse(parseInt(course))
+      .then((data) => {
+        res.json(data);
+      })
+      .catch(() => res.json({ message: "no results" }));
+  } else {
+    collegeData.getAllStudents()
+      .then((students) => res.json(students))
+      .catch(() => res.json({ message: "no results" }));
+  }
+});
+
+// Route to get all TAs
+app.get("/tas", (req, res) => {
+  collegeData.getTAs()
+    .then((TAs) => res.json(TAs))
+    .catch(() => res.json({ message: "no results" }));
+});
+
+// Route to get all courses
+app.get("/courses", (req, res) => {
+  collegeData.getCourses()
+    .then((courses) => res.json(courses))
+    .catch(() => res.json({ message: "no results" }));
+});
+
+// Route to get a student by student number
+app.get("/student/:num", (req, res) => {
+  const num = req.params.num;
+  collegeData.getStudentByNum(parseInt(num))
+    .then((student) => res.json(student))
+    .catch(() => res.json({ message: "no results" }));
+});
+
+// Route to serve home.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+// Route to serve about.html
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+// Route to serve htmlDemo.html
+app.get("/htmlDemo", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "htmlDemo.html"));
+});
+
+// Route to serve addStudent.html
+app.get("/students/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "addStudent.html"));
+});
+
+// Route to add a new student
+app.post("/students/add", (req, res) => {
+  const newStudent = req.body;
+  collegeData
+    .addStudent(newStudent)
+    .then(() => {
+      res.redirect("/students");
+    })
+    .catch(() => res.json({ message: "Failed to add student" }));
+});
+
+// 404 error message
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+// Initializing the collegeData module
+collegeData
+  .initialize()
+  .then(() => {
+    // Start the server
+    app.listen(HTTP_PORT, () => {
+      console.log("Server listening on port: " + HTTP_PORT);
+    });
+  })
+  .catch((err) => {
+    console.error("Error initializing collegeData:", err);
+  });
